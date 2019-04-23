de98c72c66b4e81270673c92d622b9e802a37593
callistagloss <49454796+callistagloss@users.noreply.github.com>
2019-04-22 20:45:14 -0400

    Add files via upload

---
+++ Lab2 Work1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            float Length;
+            float Width;
+            float Area;
+            float Perimeter;
+            float Height;
+            float Volume;
+            char Continue;
+
+            Console.WriteLine("Enter Height:");
+            Height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Length:");
+            Length = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Width:");
+            Width = float.Parse(Console.ReadLine());
+        
+            Area = (Length * Width);
+            Console.WriteLine("Area: " + Area);
+
+            Console.WriteLine("Continue? (y/n)");
+            //Continue = char.Parse(Console.ReadLine());
+            Continue = Console.ReadKey().KeyChar;
+            if(Continue == 'Y' || Continue == 'y')
+            {
+                Console.WriteLine("\nEnter Height:");
+                Height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Length:");
+                Length = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width:");
+                Width = float.Parse(Console.ReadLine());
+
+                Area = (Length * Width);
+                Console.WriteLine("Area: " + Area);
+
+            }
+
+            Perimeter = (2 * Length) + (2 * Width);
+            Console.WriteLine("Perimeter: " + Perimeter);
+
+            Volume = (Length * Width * Height);
+            Console.WriteLine("Volume: " + Volume);
+
+            Console.ReadKey();
+        }
+    }
+}
